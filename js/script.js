dce4a7fcc0ac538fe0dbdbee275e5f4a3f229652
nicolonedibari <162788540+nicolonedibari@users.noreply.github.com>
2025-02-25 09:17:12 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let url = "https://randomuser.me/api/";
+let globalPerson;
+
+fetch(url).then(
+    (response) => {
+        return response.json();
+    }
+).then(
+    function(data){
+        globalPerson = data.results[0];
+        immagine.src = globalPerson.picture;
+        nome.innerHTML = globalPerson.name.first + " " + globalPerson.name.last;
+        genere.innerHTML = globalPerson.gender;
+        indirizzo.innerHTML = globalPerson.address;
+        email.innerHTML = globalPerson.email;
+
+        console.log(data);
+    }
+).catch(
+    function(error){
+        console.log("Errore nella richiesta API:", error);
+    }
+)
+
+let immagine = document.querySelector("#user-image");
+let nome = document.querySelector("#user-name");
+let genere = document.querySelector("#user-gender");
+let indirizzo = document.querySelector("#user-address");
+let email = document.querySelector("#user-email");
